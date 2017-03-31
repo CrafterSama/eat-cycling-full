ed76f479cae5c6b390ad85994b83663aa42d0dba
Julmer Olivero <jolivero.03@gmail.com>
2017-03-31 03:54:23 -0400

    Arreglada la API, Clima Funcionando

--- js/key.js
+++ js/key.js
@@ -1,7 +1,6 @@
 $.ajaxSetup({
     headers: {
-        'apiKey': '75bchbce7y3g673b7uwh3489y547rhdiw',
-        'cache-control': 'no-cache',
-        'postman-token': 'd2ec876f-26a2-8182-28a4-6d77ecf834cc'
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'ApiKey': '75bchbce7y3g673b7uwh3489y547rhdiw'
     }
 });
--- js/weather.js
+++ js/weather.js
@@ -3,58 +3,68 @@ $('#place').change( function() {
     var place = $(this).val(),
     //localStorage.setItem('place', place);
         key = '75bchbce7y3g673b7uwh3489y547rhdiw';
-    console.log(place);
-    /*$.ajaxSetup({
-        headers: {
-            'content-type': 'application/json',
-            'apikey': key,
+        function weather() {
+            $.ajax({
+                url: 'http://api.eatandcheckcycling.com/get_weather',
+                method: 'GET',
+                data: {
+                    lat: lat,
+                    long: long,
+                    lang: 'es'
+                }
+            })
+            .done(function(data) {
+                console.log(data);
+                $('#weather', function() {
+                    $('span#temperature').html(data.weatherDay1.temperature + 'C');
+                });
+                $('#header-weather').append('<img id="image" src="img/Weather/' + data.weatherDay1.image + '.png">');
+                var main = data.weatherDay1.weather[0].main;
+                $('#weather', function() {
+                    $('span#clouds').html(main);
+                });
+                console.log(main);
+                /*if( main == 'Clouds' ) {
+                    $('#weather', function() {
+                        $('span#clouds').html('Nublado');
+                    });
+                }
+                if( main == 'Clear' ) {
+                    $('#weather', function() {
+                        $('span#clouds').html('Soleado');
+                    });
+                }
+                if( main == 'Rain' ) {
+                    $('#weather', function() {
+                        $('span#clouds').html('Lluvioso');
+                    });
+                }*/
+
+            })
+            .fail(function(jqXHR, textStatus) {
+                console.log( textStatus,'Error tratando de acceder a los datos del servicio' );
+            });
         }
-    });*/
+    console.log(place);
     if(place == 1) {
         // http://api3.eatandcheckcycling.com/get_weather?lat=39.529942&long=2.729396
         var lat = 39.529942,
-            long = 2.729396;
-            $('#weather', function(){
-                $('#city').html('Mallorca');
-            });
-        $.ajax({
-            url: 'http://api.eatandcheckcycling.com/get_weather',
-            async: true,
-            crossDomain: true,
-            method: "GET",
-            data: {
-                lat: lat,
-                long: long
-            }
-        })
-        .done(function(data) {
-            console.log(data);
-        })
-        .fail(function(jqXHR, textStatus) {
-            console.log( textStatus,'Error tratando de acceder a los datos del servicio' );
+            long = 2.729396,
+            url = 'http://api.eatandcheckcycling.com/get_weather?lat=' + lat + '&long=' + long;
+        $('#weather', function() {
+            $('#city').html('Mallorca');
         });
+        weather();
     }
     else {
-        var lat = 28.292249,
-            long = -17.0831152;
-            $('#weather', function(){
-                $('#city').html('Tenerife');
-            });
-        $.ajax({
-            url: 'http://api.eatandcheckcycling.com/get_weather',
-            async: true,
-            crossDomain: true,
-            method: "GET",
-            data: {
-                lat: lat,
-                long: long
-            }
-        })
-        .done(function(data) {
-            console.log(data);
-        })
-        .fail(function(jqXHR, textStatus) {
-            console.log( textStatus,'Error tratando de acceder a los datos del servicio' );
+        //var lat = 28.292249,
+        var lat = 60.1269612,
+            //long = -17.0831152;
+            long = -176.4151934;
+            url = 'http://api.eatandcheckcycling.com/get_weather?lat=' + lat + '&long=' + long;
+        $('#weather', function(){
+            $('#city').html('Tenerife');
         });
+        weather();
     }
 });
